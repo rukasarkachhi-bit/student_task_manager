4859392aebce99322c863d305b5c25700659a752
rukasarkachhi-bit <rukasarkachhi@gmail.com>
2026-06-11 00:09:14 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,8 +3,8 @@ import mysql.connector
 def get_database_connection():
     connection = mysql.connector.connect(
         host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-        user='L5pS1NJTn8X6Ekz.root',
-        password='QZ5bRIrOdhTijxG7',
+        user='3CcZnT4CoGr6epo.root',
+        password='i8RpOtWsMU7pmMYf',
         database='student_task_manager',
         port=4000
     )
